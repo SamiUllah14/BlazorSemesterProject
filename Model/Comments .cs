f83af47b443b3c9a88ef8921016cf801e89147cb
Sami Afzal <egtsami@gmail.com>
2024-01-16 04:55:20 +0500

    Add project files.

---
+++ Model/Comments .cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSemesterProject.Model
+{
+   
+    
+        public class Comments
+        {
+            [Key]
+            public int Id { get; set; } = 1;
+
+            public string Description { get; set; }
+        }
+    }
+
+
+
